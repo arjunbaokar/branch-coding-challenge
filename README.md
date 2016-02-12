1c06125c95bd20763d993384c1bf91f95b7cadb9
Arjun Baokar <arjunbaokar@gmail.com>
2016-02-11 17:32:25 -0800

    read all of aws and put it into mongodb

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # branch-coding-challenge
+
+- Put AWS credentials into a file called 'aws_credentials', where the first line is AWS_ACCESS_KEY_ID, and the second is AWS_SECRET_ACCESS_KEY. Nothing else should be in the file. My file is not pushed to the repo, but make sure you add in aws_credentials or it won't be able to grab the data from AWS.
--- get_data.py
+++ get_data.py
@@ -1,17 +1,44 @@
 import os
 import boto
+import json
 from pymongo import MongoClient
 
-with open('aws_credentials') as f:
-	os.environ['AWS_ACCESS_KEY_ID'] = f.readline().strip()
-	os.environ['AWS_SECRET_ACCESS_KEY'] = f.readline().strip()
+class DataManager:
 
+	def __init__(self):
+		self.client = MongoClient()
+		self.set_credentials()
 
-# client = MongoClient()
-# db = client.branchtest
+		self.db = self.client.branch_challenge
+		self.s3_conn = boto.connect_s3()
+		self.bucket = self.s3_conn.get_bucket('branch-test-warehouse')
 
-s3_conn = boto.connect_s3()
-bucket = s3_conn.get_bucket('branch-test-warehouse')
-rs = s3_conn.get_all_buckets()
-for b in rs:
-	print b.name
+	# Sets AWS credentials so it can read stuff
+	def set_credentials(self):
+		with open('aws_credentials') as f:
+			os.environ['AWS_ACCESS_KEY_ID'] = f.readline().strip()
+			os.environ['AWS_SECRET_ACCESS_KEY'] = f.readline().strip()
+
+	# Call this after instantiating DataManager object to populate the DB.
+	def populate_db(self):
+		user_data_keys = self.bucket.get_all_keys(prefix='uploads/users')
+
+		for key in user_data_keys:
+			key_split = filter(None, key.name.split('/'))
+			if len(key_split) > 5:
+				user_id = key_split[2]
+				key_type = key_split[4]
+
+				contents = key.get_contents_as_string()
+				contents = json.loads(contents)
+				for d in contents:
+					d['uid'] = user_id
+					d['key_type'] = key_type
+
+				try:
+					db_result = self.db.test.insert_many(contents)
+				except:
+					print "Empty: ", key_split
+
+dm = DataManager()
+dm.populate_db()
